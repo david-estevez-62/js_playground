387246822d904183803ee3d8dc3db899760adae8
Eric <ejh81491@gmail.com>
2016-12-31 16:57:15 -0500

    input output sites

---
+++ richTextEditor/main.js
@@ -0,0 +1,46 @@
+function iFrameOn(){
+  richTextField.document.designMode = "On";
+}
+function iBuild(){
+  richTextField.document.execCommand("bold", false, null)
+}
+function iUnderline(){
+  richTextField.document.execCommand("underline", false,null)
+}
+function iItalic(){
+  richTextField.document.execCommand("italic",false,null)
+}
+function iFontSize(){
+  var size = prompt("Enter a size 1 -7", '');
+  richTextField.document.execCommand("FontSize",false,size)
+}
+function iForeColor(){
+  var color = prompt("Define a basic color or apply a hexadecimal color more advanced colors:", "");
+}
+function iHorizontalRule(){
+  richTextField.document.execCommand("inserthorizontalrule",false,null);
+}
+function iUnorderedList(){
+  richTextField.document.execCommand("InsertOrderedList", false, "newOL");
+}
+function iOrderedList(){
+  richTextField.document.execCommand("InsertUnorderedList", false,"newUL");
+}
+function iLink(){
+  var linkURL = prompt("Enter the URL for this link:", "http://");
+  richTextField.document.execCommand("CreateLink", false, linkURL);
+}
+function iUnlink(){
+  richTextField.document.execCommand("Unlink", false, null);
+}
+function iImage(){
+  var imgSrc = prompt("Enter image location", "");
+  if(imgSrc != null) {
+    richTextField.document.execCommand("insertimage",false,imgSrc);
+  }
+}
+function submit_form(){
+  var theForm = document.getElementById("myform");
+  theForm.elements["myTextArea"].value = window.frames["richTextField"].document.body.innerHTML;
+  theForm.submit();
+}
